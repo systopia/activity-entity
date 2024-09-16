046c989e723fde3351ec92b3a50fd4d05a3d976b
Dominic Tubach <tubach@systopia.de>
2024-09-16 13:56:59 +0200

    chore: civix upgrade

--- activity_entity.php
+++ activity_entity.php
@@ -36,57 +36,10 @@ function activity_entity_civicrm_install(): void {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function activity_entity_civicrm_postInstall(): void {
-  _activity_entity_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function activity_entity_civicrm_uninstall(): void {
-  _activity_entity_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function activity_entity_civicrm_enable(): void {
   _activity_entity_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function activity_entity_civicrm_disable(): void {
-  _activity_entity_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function activity_entity_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _activity_entity_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function activity_entity_civicrm_entityTypes(&$entityTypes): void {
-  _activity_entity_civix_civicrm_entityTypes($entityTypes);
 }
