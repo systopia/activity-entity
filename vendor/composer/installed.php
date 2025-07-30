c1c1434c588f1f3355895c148ebfea1d153e3d99
Dominic Tubach <tubach@systopia.de>
2025-07-30 09:45:12 +0200

    Set version to 1.0.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/activity-entity',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5cf8bc06656171b90d65030dfa17589b74a3f6a5',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/activity-entity' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5cf8bc06656171b90d65030dfa17589b74a3f6a5',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'webmozart/assert' => array(
+            'pretty_version' => '1.11.0',
+            'version' => '1.11.0.0',
+            'reference' => '11cb2199493b2f8a3b53e7f19068fc6aac760991',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../webmozart/assert',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
